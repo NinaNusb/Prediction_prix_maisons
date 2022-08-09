22ba13093e6d60d3856db15f83b52fc373c3cd8d
NinaNusb <nina.nusbaumer@gmail.com>
2022-08-09 10:52:46 +0200

    structure app fonctionnelle

--- app.py
+++ app.py
@@ -15,11 +15,15 @@ df= pd.read_csv("data_cleaning_analysis.csv")
 # Titre de la page
 st.title('House Sales in King County, USA')
 
-# zipcode
-st.number_input("Entrez le code postal du quartier dans lequel vous cherchez une maison")
-st.number_input()
+#Header
+st.header("Vous cherchez une maison dans la ville de Seattle? Ajustez les paramètres ci-dessous pour découvrir la prédiction de votre future maison.")
 
-st.subheader('Map of all neighborhoods')
+# zipcode
+st.subheader('Code postal')
+st.number_input("Entrez le code postal du quartier dans lequel vous cherchez une maison")
+
+
+st.subheader('Carte de tous les quartiers')
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [47.7776, -121.315],
     columns=['lat', 'lon'])
@@ -27,21 +31,45 @@ map_data = pd.DataFrame(
 st.map(map_data)
 
 # Nb chambres
+st.subheader('Nombre de chambre')
 bedrooms =  st.slider("Choisissez un nombre", 0, 10)
+st.text('Sélection: {}'.format(bedrooms))
 
 # Nb sdb
-bathrooms =  st.slider("Choisissez un nombre", 0, 8)
-
-# Sqft_living
-sqft_living =  st.slider("Choisissez un nombre", 0, 13540)
-
-# Sqft_lot
-sqft_lot =  st.slider("Choisissez un nombre", 0, 1651359)
+st.subheader('Nombre de salle-de-bains')
+bathrooms =  st.slider("Choisissez un nombre", 0, 8) 
+st.text('Sélection: {}'.format(bathrooms))
+# [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5]
 
 # floors
-floors =  st.slider("Choisissez un nombre", 0, 3.5)
+st.subheader("Nombre d'étages")
+floors =  st.selectbox("Choisissez un nombre", [0, 1, 1.5, 2, 2.5, 3, 3.5])
+st.write("Vous avez choisi ", floors, " étages")
+
+# Condition
+st.subheader("Qualité intérieure")
+condition = st.radio(
+    "Choisissez le niveau de la condition",
+    (1, 2, 3, 4, 5)
+)
+
+# Grade
+st.subheader("Qualité de la structure")
+grade = st.slider("Choisissez un nombre", 0, 13)
+
+
+# Sqft_living
+st.subheader('Surface habitable en pieds carrés')
+sqft_living =  st.slider("Choisissez un nombre", 0, 13540)
+st.text('Sélection: {}'.format(sqft_living))
+
+# Sqft_lot
+st.subheader('Surface du terrain en pieds carrés')
+sqft_lot =  st.slider("Choisissez un nombre", 0, 1651359)
+st.text('Sélection: {}'.format(sqft_lot))
 
 # Waterfront
+st.subheader("Proximité à l'eau")
 waterfront = st.radio(
      "Voulez-vous être au bord de l'eau?",
      ('Oui', 'Non'))
@@ -53,27 +81,25 @@ else:
 
 
 # View
+st.subheader("Qualité de la vue")
 view = st.radio(
     "Choisissez le niveau de la vue",
     (1, 2, 3, 4)
 )
 
-# Condition
-condition = st.radio(
-    "Choisissez le niveau de la condition",
-    (1, 2, 3, 4, 5)
-)
-
-# Grade
-grade = st.slider("Choisissez un nombre", 0, 13)
-
-# sqft_above
-sqft_above = st.slider("Choisissez un nombre", 0, 9410)
-
 # yr_built
+st.subheader("Année de construction")
 yr_built = st.slider("Choisissez un nombre", 1900, 2015)
 
 # yr_renovated
-yr_renovated = st.write("Choisissez un nombre")
+st.subheader("Année de rénovation")
+st.text("Si vous ne souhaitez pas de maison rénovée, indiquez 0")
+yr_renovated = st.number_input("Choisissez un nombre")
 
+
+# Trouver la prédiction
 st.button("Trouver")
+
+if(st.button("Trouver")):
+    # prix = ??
+    st.success(prix)
